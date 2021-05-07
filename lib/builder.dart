9113dcfc3026a948d02eb15d75ecf31c93d6bf93
Jonathan Vegas Peixoto <jopxoto12@gmail.com>
2021-05-07 16:30:17 -0300

    1.1.3

--- lib/builder.dart
+++ lib/builder.dart
@@ -1,8 +1,9 @@
 library data_class_builder.builder;
 
 import 'package:build/build.dart';
-import 'package:data_class_builder/src/data_class_builder.dart';
 import 'package:source_gen/source_gen.dart';
+
+import 'src/data_class_builder.dart';
 
 Builder dataClassBuilder(BuilderOptions options) => PartBuilder(
       const [DataClassBuilder()],
